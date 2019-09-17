91fdf6fe67ec54df3d9316ea3dcab820c1d7c796
itiievsk <itiyevskiy@gmail.com>
2019-09-17 23:37:06 +0300

    fixes

--- pyker/utils/loading.py
+++ pyker/utils/loading.py
@@ -1,10 +1,11 @@
-from typing import List
 import pkgutil
-from pyker import generators
 from importlib import import_module
+from typing import List
+
+from pyker import generators
 
 
-def get_module_names(path: str) -> List[str]:
+def get_module_names() -> List[str]:
     return [
         name
         for _, name, is_pkg in pkgutil.walk_packages(path=generators.__path__)
@@ -13,5 +14,5 @@ def get_module_names(path: str) -> List[str]:
 
 
 def get_all_modules(path: str):
-    module_names = get_module_names(path)
+    module_names = get_module_names()
     return set([import_module(f"{path}.{name}") for name in module_names])
